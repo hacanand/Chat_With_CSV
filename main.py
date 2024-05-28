609ab7837313a0c650f603978cfb1ff340a4ef5a
anand <anandpromax@gmail.com>
2024-05-28 11:40:12 +0530

    chore: Refactor code to use Google Generative AI for chat responses

--- main.py
+++ main.py
@@ -3,10 +3,12 @@ import streamlit as st
 from langchain_experimental.agents.agent_toolkits import create_csv_agent
 from dotenv import load_dotenv
 # from langchain import ChatOpenAI
-from langchain_community.chat_models import ChatOpenAI
+# from langchain_community.chat_models import ChatOpenAI
+from langchain_google_genai import ChatGoogleGenerativeAI
 load_dotenv()
 def llm_response(query):
-    llm=ChatOpenAI(temperature=0, model="gpt-3.5-turbo-0613", openai_api_key= os.environ["OPENAI_API_KEY"])
+    # llm=ChatOpenAI(temperature=0, model="gpt-3.5-turbo-0613", openai_api_key= os.environ["OPENAI_API_KEY"])
+    llm=ChatGoogleGenerativeAI(model="gemini-1.5-pro",google_api_key=os.environ["GOOGLE_API_KEY"],temperature=0.5)
     agent_executer=create_csv_agent(llm, 'salaries.csv', verbose=True,return_intermediate_steps=True)
     response = agent_executer.invoke(query)
     return response
@@ -18,7 +20,11 @@ def ChatWithCsvGui():
         st.write('This is a chat interface that uses a given CSV file as a knowledge base.')
         user_question = st.text_input('Ask a question about the CSV file:')
         if st.button('Submit'):
-            res=llm_response(user_question)
-            st.write(res)
+            if not user_question:
+                st.warning('Please input a valid text.')
+                st.stop()
+            else:
+                res=llm_response(user_question)
+                st.write(res)
 
 ChatWithCsvGui()
